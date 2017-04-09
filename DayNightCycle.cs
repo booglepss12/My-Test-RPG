eb5b9c7a386e1cdb8a0c980ab63de9d8e19ef7c0
Unknown <grooney4@gmail.com>
2017-04-09 19:41:01 -0400

    Added in trees and temporary bridge

---
+++ DayNightCycle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.RotateAround (Vector3.zero, Vector3.right, 1.5f*Time.deltaTime); //rotate around zero point along the right axis at the speed
+		transform.LookAt (Vector3.zero);
+
+
+	}
+}
