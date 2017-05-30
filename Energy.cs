5c6142615492c074e91be5127ab17c33f101e3dc
Unknown <grooney4@gmail.com>
2017-05-29 20:38:05 -0400

    Refactor of CameraRaycaster.cs, PlayerMovement.cs to simplify

--- Energy.cs
+++ Energy.cs
@@ -1,44 +1,99 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 using UnityEngine.UI;
+
 using RPG.CameraUI;
 
+
+
 namespace RPG.Characters
+
 {
+
     public class Energy : MonoBehaviour
+
     {
+
         [SerializeField] RawImage energyBar;
+
         [SerializeField] float maxEnergyPoints = 100f;
+
         [SerializeField] float pointsPerHit = 10f;
 
+
+
         float currentEnergyPoints;
-        CameraRaycaster cameraRaycaster;
+
+        CameraUI.CameraRaycaster cameraRaycaster;
+
+
+
         // Use this for initialization
+
         void Start()
+
         {
+
             currentEnergyPoints = maxEnergyPoints;
-            cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
-            cameraRaycaster.notifyRightClickObservers += ProcessRightClick;
+
+            cameraRaycaster = Camera.main.GetComponent<CameraUI.CameraRaycaster>();
+
+            cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
+
         }
 
-        
-        void ProcessRightClick (RaycastHit raycastHit, int layerHit)
+
+
+        void OnMouseOverEnemy(Enemy enemey)
+
         {
-            float newEnergyPoints = currentEnergyPoints - pointsPerHit;
-            currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0, maxEnergyPoints);
-            UpdateEnergyBar();
+
+            if (Input.GetMouseButtonDown(1))
+
+            {
+
+                UpdateEnergyPoints();
+
+                UpdateEnergyBar();
+
+            }
+
         }
+
+
+
+        private void UpdateEnergyPoints()
+
+        {
+
+            float newEnergyPoints = currentEnergyPoints - pointsPerHit;
+
+            currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0, maxEnergyPoints);
+
+        }
+
+
 
         private void UpdateEnergyBar()
+
         {
-            float xValue = -(EnergyAsPercent()/ 2f) - 0.5f;
+
+            float xValue = -(EnergyAsPercent() / 2f) - 0.5f;
+
             energyBar.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+
         }
-        
+
+
+
         float EnergyAsPercent()
+
         {
+
             return currentEnergyPoints / maxEnergyPoints;
+
         }
+
     }
+
 }
--- _Characters/Player.cs
+++ _Characters/Player.cs
@@ -1,107 +1,201 @@
 ﻿using System;
+
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
+
 using UnityEngine.Assertions;
 
+
+
 // TODO consider re-wire...
+
 using RPG.CameraUI;
+
 using RPG.Core;
+
 using RPG.Weapons;
 
+
+
 namespace RPG.Characters
+
 {
+
     public class Player : MonoBehaviour, IDamageable
+
     {
-        [SerializeField] int enemyLayer = 9;
+
         [SerializeField] float maxHealthPoints = 100f;
+
         [SerializeField] float damagePerHit = 10f;
-        [SerializeField] Weapon weaponInUse;
-        [SerializeField] AnimatorOverrideController animatorOverrideController;
+
+        [SerializeField] Weapon weaponInUse = null;
+
+        [SerializeField] AnimatorOverrideController animatorOverrideController = null;
+
+
 
         Animator animator;
+
         float currentHealthPoints;
+
         CameraRaycaster cameraRaycaster;
+
         float lastHitTime = 0f;
+
+
 
         public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; } }
 
+
+
         void Start()
+
         {
+
             RegisterForMouseClick();
+
             SetCurrentMaxHealth();
+
             PutWeaponInHand();
+
             SetupRuntimeAnimator();
+
         }
+
+
 
         public void TakeDamage(float damage)
+
         {
+
             currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+
         }
+
+
 
         private void SetCurrentMaxHealth()
+
         {
+
             currentHealthPoints = maxHealthPoints;
+
         }
+
+
 
         private void SetupRuntimeAnimator()
+
         {
+
             animator = GetComponent<Animator>();
+
             animator.runtimeAnimatorController = animatorOverrideController;
+
             animatorOverrideController["DEFAULT ATTACK"] = weaponInUse.GetAttackAnimClip(); // remove const
+
         }
+
+
 
         private void PutWeaponInHand()
+
         {
+
             var weaponPrefab = weaponInUse.GetWeaponPrefab();
+
             GameObject dominantHand = RequestDominantHand();
+
             var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+
             weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+
             weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+
         }
+
+
 
         private GameObject RequestDominantHand()
+
         {
+
             var dominantHands = GetComponentsInChildren<DominantHand>();
+
             int numberOfDominantHands = dominantHands.Length;
+
             Assert.IsFalse(numberOfDominantHands <= 0, "No DominantHand found on Player, please add one");
+
             Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHand scripts on Player, please remove one");
+
             return dominantHands[0].gameObject;
+
         }
+
+
 
         private void RegisterForMouseClick()
+
         {
-            cameraRaycaster = FindObjectOfType<CameraRaycaster>();
-            cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
+
+            cameraRaycaster = FindObjectOfType<CameraUI.CameraRaycaster>();
+
+            cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
+
         }
 
-        void OnMouseClick(RaycastHit raycastHit, int layerHit)
+
+
+        void OnMouseOverEnemy(Enemy enemy)
+
         {
-            if (layerHit == enemyLayer)
+
+            if (Input.GetMouseButton(0) && IsTargetInRange(enemy.gameObject))
+
             {
-                var enemy = raycastHit.collider.gameObject;
-                if (IsTargetInRange(enemy))
-                {
-                    AttackTarget(enemy);
-                }
+
+                AttackTarget(enemy);
+
             }
+
         }
 
-        private void AttackTarget(GameObject target)
+
+
+        private void AttackTarget(Enemy enemy)
+
         {
-            var enemyComponent = target.GetComponent<Enemy>();
+
             if (Time.time - lastHitTime > weaponInUse.GetMinTimeBetweenHits())
+
             {
+
                 animator.SetTrigger("Attack"); // TODO make const
-                enemyComponent.TakeDamage(damagePerHit);
+
+                enemy.TakeDamage(damagePerHit);
+
                 lastHitTime = Time.time;
+
             }
+
         }
+
+
 
         private bool IsTargetInRange(GameObject target)
+
         {
+
             float distanceToTarget = (target.transform.position - transform.position).magnitude;
+
             return distanceToTarget <= weaponInUse.GetMaxAttackRange();
+
         }
+
     }
+
 }
