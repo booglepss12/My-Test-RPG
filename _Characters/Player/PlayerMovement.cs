18fe89169d5d1b9da839bc67fcebcfc502490914
Unknown <grooney4@gmail.com>
2017-08-20 11:13:23 -0400

    Changed enemy to enemyAI

--- _Characters/Player/PlayerMovement.cs
+++ _Characters/Player/PlayerMovement.cs
@@ -11,7 +11,7 @@ namespace RPG.Characters
         [SerializeField] float criticalHitMultiplier = 1.25f;
 		[SerializeField] ParticleSystem criticalHitParticle = null;
 
-        Enemy enemy = null;
+        EnemyAI enemy = null;
 
         Character character;
         SpecialAbilities abilities;
@@ -59,7 +59,7 @@ namespace RPG.Characters
             }
         }
 
-        void OnMouseOverEnemy(Enemy enemyToSet)
+        void OnMouseOverEnemy(EnemyAI enemyToSet)
         {
             this.enemy = enemyToSet;
             if (Input.GetMouseButton(0) && IsTargetInRange(enemy.gameObject))
---
+++ _Characters/_Enemies/EnemyAI.cs
@@ -0,0 +1,51 @@
+﻿﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RPG.Core; // TODO consider re-wire
+
+namespace RPG.Characters
+{
+    [RequireComponent (typeof(WeaponSystem))]
+    public class EnemyAI : MonoBehaviour
+    { 
+        //TODO remove
+        [SerializeField] float chaseRadius = 6f;
+        
+
+        bool isAttacking = false; //TODO a more rich state
+        PlayerMovement player = null;
+        float currentWeaponRange;
+
+        void Start()
+        {
+            player = FindObjectOfType<PlayerMovement>();
+        }
+
+        public void TakeDamage(float amount)
+        {
+            // todo remove
+        }
+
+        void Update()
+        {
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            WeaponSystem weaponSystem = GetComponent<WeaponSystem>();
+            currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
+            
+        }
+
+        
+
+        void OnDrawGizmos()
+        {
+            // Draw attack sphere 
+            Gizmos.color = new Color(255f, 0, 0, .5f);
+            Gizmos.DrawWireSphere(transform.position, currentWeaponRange);
+
+            // Draw chase sphere 
+            Gizmos.color = new Color(0, 0, 255, .5f);
+            Gizmos.DrawWireSphere(transform.position, chaseRadius);
+        }
+    }
+}
